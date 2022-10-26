01f91527cf9851da298f58b43a7ea5a118a888f1
Ferrozo <ferrozo.c@gmail.com>
2022-10-26 16:40:57 +0100

    first lesson done

--- implicit_animations/lesson_01/README.md
+++ implicit_animations/lesson_01/README.md
@@ -1,16 +1,8 @@
 # lesson_01
 
-A new Flutter project.
+First Lesson about animated Widget on flutter.
+## on press button out put:
 
-## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+<div>
+    <img src='./animated_01.gif' width='280px'>
+</div>
---
+++ implicit_animations/lesson_01/lib/src/presentation/screen/home/home_screen.dart
@@ -0,0 +1,58 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+// ignore: must_be_immutable
+class HomePage extends StatefulWidget {
+  HomePage({Key? key, required this.title}) : super(key: key);
+  String title;
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  double _height = 100.0;
+  double _width = 100.0;
+  Color _containerColor = Colors.red;
+  BorderRadiusGeometry _containerBorderRadius = BorderRadius.circular(20);
+
+  final randomValue = Random();
+
+  void _randomShape() {
+    setState(() {
+      _width = randomValue.nextInt(150).toDouble();
+      _height = randomValue.nextInt(150).toDouble();
+      _containerColor = Color.fromRGBO(
+        randomValue.nextInt(780),
+        randomValue.nextInt(780),
+        randomValue.nextInt(780),
+        1,
+      );
+      _containerBorderRadius =
+          BorderRadius.circular(randomValue.nextInt(50).toDouble());
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: AnimatedContainer(
+          height: _height,
+          width: _width,
+          decoration: BoxDecoration(
+            color: _containerColor,
+            borderRadius: _containerBorderRadius,
+          ),
+          duration: const Duration(seconds: 2),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: _containerColor,
+        onPressed: () => _randomShape(),
+        child: const Icon(Icons.dynamic_feed),
+      ),
+    );
+  }
+}
